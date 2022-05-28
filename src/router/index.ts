9b0ccebff23373a3de95a5d15cb179f1bbce7dae
yusuke_ando <ando08620@gmail.com>
2022-05-29 01:52:35 +0900

    修正

--- src/router/index.ts
+++ src/router/index.ts
@@ -65,7 +65,7 @@ const routes: Array<RouteRecordRaw> = [
 const routerBase = createMemoryHistory(baseUrl);
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
