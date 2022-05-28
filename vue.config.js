6b4da999be4ace1999d276c526fae890a00468b0
yusuke_ando <ando08620@gmail.com>
2022-05-29 00:19:49 +0900

    修正

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-    publicPath: "./",
+    publicPath: "ando_portfolio",
     outputDir: "docs",
     assetsDir: "./",
     transpileDependencies: true,
